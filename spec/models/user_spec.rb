e2d466679b84922dd6bc9893da88e95f59cd2e05
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-02-02 07:25:55 -0800

    Add initial user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    user = build(:user)
+    expect(user.valid?).to be true
+  end
+
+  it "saves password" do
+    user = create(:user, password: "supersecret", password_confirmation: "supersecret")
+    expect(user.authenticate('wrong_password')).to be false
+    expect(user.authenticate('supersecret')).to eq user
+  end
 end
