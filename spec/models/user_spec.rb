cb2c448e87cd512d05dd434bd9f067bbbcf34c2d
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-02-02 08:55:47 -0800

    Add specs for unique email, downcase email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,4 +19,16 @@ RSpec.describe User, type: :model do
     user.update!(email: "new@example.com")
     expect(user.auth_token).to eq token
   end
+
+  it "has unique email" do
+    user = create(:user)
+    new_user = build(:user, email: user.email)
+    expect(new_user.valid?).to be false
+  end
+
+  it "saves email as downcase" do
+    user = build(:user, email: "FOO@eXaMpLe.COM")
+    user.save
+    expect(user.email).to eq "foo@example.com"
+  end
 end
