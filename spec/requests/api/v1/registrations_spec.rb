3ec667c80ce51c3f991538d3c3378a43a8b0f5f4
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-02-02 15:21:15 -0800

    Stub registration#create spec

--- spec/requests/api/v1/registrations_spec.rb
+++ spec/requests/api/v1/registrations_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "API::V1::Registrations", type: :request do
+  describe "POST #create" do
+    let(:path) { "/v1/sign_up" }
+
+    context "with valid params" do
+      let(:params) {
+        {
+          email: "foo@example.com",
+          password: "supersecret",
+          password_confirmation: "supersecret",
+        }
+      }
+
+      it "increases user count" do
+      end
+
+      it "returns HTTP status success" do
+      end
+
+      it "returns user info in response" do
+      end
+    end
+
+    context "with invalid params" do
+      it "does not increase user count" do
+      end
+
+      it "returns HTTP 422 status" do
+      end
+
+      it "returns error info in response" do
+      end
+    end
+  end
 end
