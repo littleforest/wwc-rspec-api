5503b626e21e1069930c237d0bf08715dc7ffcd3
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-02-02 16:46:00 -0800

    Fill in registrations#create method

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,4 +1,17 @@
 class API::V1::RegistrationsController < API::V1::APIController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { error: @user.errors.full_messages.to_sentence },
+        status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
