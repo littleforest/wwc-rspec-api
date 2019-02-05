792d7fc02d19a8de36c4a5aa7050fcae97a8b842
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-02-04 21:47:34 -0800

    Add route for sessions#create method

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, path: "", defaults: { format: :json } do
     namespace :v1 do
       post "sign_up" => "registrations#create"
+      post "sign_in" => "sessions#create"
     end
   end
 end
