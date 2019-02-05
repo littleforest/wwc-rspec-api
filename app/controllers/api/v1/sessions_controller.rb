c14b6007b16b48e62ea63f65911d0008264bb7ff
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-02-04 22:00:21 -0800

    Add spec and controller for sign in

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,11 @@
+class API::V1::SessionsController < API::V1::APIController
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      render json: @user, root: API_ROOT
+    else
+      render json: { error: "Invalid email or password" },
+        status: :unprocessable_entity
+    end
+  end
+end
---
+++ spec/requests/api/v1/sessions_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe "API::V1::Sessions", type: :request do
+  describe "POST #create" do
+    let(:path) { "/v1/sign_in" }
+    let(:user) { create(:user) }
+
+    context "with valid params" do
+      let(:valid_params) {
+        {
+          email: user.email,
+          password: "supersecret",
+        }
+      }
+
+      it "returns HTTP status success" do
+        post path, params: valid_params
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns user info in response" do
+        post path, params: valid_params
+        expect(json["payload"].keys).to match_array(%w(id email auth_token username))
+        expect(json["payload"]["id"]).to_not be nil
+        expect(json["payload"]["email"]).to eq user.email
+        expect(json["payload"]["auth_token"]).to_not be nil
+      end
+    end
+
+    context "with invalid password" do
+      let(:invalid_params) {
+        {
+          email: user.email,
+          password: "badpassword",
+        }
+      }
+
+      it "returns HTTP 422 status" do
+        post path, params: invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "returns error info in response" do
+        post path, params: invalid_params
+        expect(json["error"]).to eq "Invalid email or password"
+      end
+    end
+
+    context "with invalid email" do
+      let(:invalid_params) {
+        {
+          email: "foo@example.com",
+          password: "supersecret",
+        }
+      }
+
+      it "returns HTTP 422 status" do
+        post path, params: invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "returns error info in response" do
+        post path, params: invalid_params
+        expect(json["error"]).to eq "Invalid email or password"
+      end
+    end
+  end
+end
