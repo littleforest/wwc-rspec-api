bd2abad88240f2f5775bb3a043a6e5e644f8e175
Frances Morales <fmorales@francesmorales.com>
2019-02-12 20:40:43 -0800

    Lint fix: Layout/EmptyLinesAroundBlockBody

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_02_02_151356) do
-
   create_table 'users', force: :cascade do |t|
     t.string 'email'
     t.string 'username'
@@ -20,5 +19,4 @@ ActiveRecord::Schema.define(version: 2019_02_02_151356) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
-
 end
