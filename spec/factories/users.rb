df72d6661c4c40307e1f6067db52cbe1f2ad5a03
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-03-03 08:54:29 -0800

    Add specs for /me endpoint

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@example.com" }
-    username { 'MyString' }
     password { 'supersecret' }
   end
 end
--- spec/requests/api/v1/profile_spec.rb
+++ spec/requests/api/v1/profile_spec.rb
@@ -18,16 +18,17 @@ RSpec.describe 'API::V1::Profile', type: :request do
     end
 
     it 'returns user info in response' do
+      get path, headers: auth_header(user)
+      expect(json['payload'].keys).to match_array(%w(id email auth_token username))
+      expect(json['payload']['id']).to eq user.id
+      expect(json['payload']['email']).to eq user.email
+      expect(json['payload']['auth_token']).to eq user.auth_token
+      expect(json['payload']['username']).to be nil
     end
   end
 
   describe 'PATCH #me' do
-    shared_examples 'authorization' do
-      it 'returns success if authorized' do
-        patch path, params: params, headers: auth_header(user)
-        expect(response).to have_http_status(:success)
-      end
-
+    shared_examples 'unauthorized' do
       it 'returns unauthorized if bad authorization' do
         patch path, params: params, headers: bad_auth_header
         expect(response).to have_http_status(:unauthorized)
@@ -35,31 +36,62 @@ RSpec.describe 'API::V1::Profile', type: :request do
     end
 
     context 'with valid params' do
-      let(:params) {}
+      let(:params) {
+        {
+          email: 'foobar@example.com',
+          username: 'foobar'
+        }
+      }
 
-      it_behaves_like 'authorization' do
+      it 'returns success if authorized' do
+        patch path, params: params, headers: auth_header(user)
+        expect(response).to have_http_status(:success)
       end
 
-      it 'updates user' do
+      it_behaves_like 'unauthorized'
 
+      it 'updates user' do
+        patch path, params: params, headers: auth_header(user)
+        user.reload
+        expect(user.email).to eq 'foobar@example.com'
+        expect(user.username).to eq 'foobar'
       end
 
       it 'returns user info in response' do
+        patch path, params: params, headers: auth_header(user)
+        expect(json['payload'].keys).to match_array(%w(id email auth_token username))
+        expect(json['payload']['id']).to eq user.id
+        expect(json['payload']['email']).to eq 'foobar@example.com'
+        expect(json['payload']['auth_token']).to eq user.auth_token
+        expect(json['payload']['username']).to eq 'foobar'
       end
     end
 
     context 'with invalid params' do
-      let(:params) {}
+      let(:params) {
+        {
+          email: '',
+          username: 'foobar'
+        }
+      }
 
-      it_behaves_like 'authorization'
+      it_behaves_like 'unauthorized'
 
       it 'returns HTTP 422 status' do
+        patch path, params: params, headers: auth_header(user)
+        expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'does not update user' do
+        patch path, params: params, headers: auth_header(user)
+        user.reload
+        expect(user.email).to_not be_blank
+        expect(user.username).to be nil
       end
 
       it 'returns error info in response' do
+        patch path, params: params, headers: auth_header(user)
+        expect(json['error']).to eq "Email can't be blank"
       end
     end
   end
