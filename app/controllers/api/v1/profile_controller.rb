e4c4f114771669a5e3058016344b9bf0255bc490
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-03-03 09:00:06 -0800

    Add /me endpoint GET and PATCH endpoints

--- app/controllers/api/v1/profile_controller.rb
+++ app/controllers/api/v1/profile_controller.rb
@@ -4,8 +4,21 @@ class API::V1::ProfileController < API::V1::APIController
   before_action :authenticate
 
   def show
+    render json: current_user, root: API_ROOT
   end
 
   def update
+    if current_user.update(update_params)
+      render json: current_user, root: API_ROOT
+    else
+      render json: { error: current_user.errors.full_messages.to_sentence},
+             root: API_ROOT, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def update_params
+    params.permit(:email, :username)
   end
 end
