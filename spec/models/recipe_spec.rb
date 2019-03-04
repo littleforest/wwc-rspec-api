3aaa947013cc49c8b457fcfe141c6a74319e6c7d
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-03-03 16:10:12 -0800

    Stub specs for recipe.retrieve_all

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Recipe, type: :model do
     recipe = build(:recipe)
     expect(recipe.valid?).to be true
   end
+
+  describe ".retrieve_all" do
+    context "when user is nil" do
+      it "returns all recipes, ordered by most recently created" do
+      end
+    end
+
+    context "when user is not nil" do
+      it "returns all recipes not created by user, ordered by most recently created" do
+      end
+    end
+  end
 end
