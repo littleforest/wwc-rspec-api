d67c5a92e92b59b42f8d75f84a4e799fb1c6c8fb
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-03-12 20:21:15 -0700

    Refactor community method

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,15 +4,10 @@ class API::V1::RecipesController < API::V1::APIController
   before_action :authenticate_token
 
   def community
-    if params[:q].present? && current_user.nil?
-      @recipes = Recipe.where(title: params[:q]).order(id: :desc)
-    elsif params[:q].present? && current_user.present?
-      @recipes = Recipe.where(title: params[:q])
-                       .where.not(user: current_user).order(id: :desc)
-    elsif current_user
-      @recipes = Recipe.where.not(user_id: current_user.id).order(id: :desc)
+    if params[:q].present?
+      @recipes = Recipe.retrieve_all(current_user).where(title: params[:q])
     else
-      @recipes = Recipe.order(id: :desc)
+      @recipes = Recipe.retrieve_all(current_user)
     end
     render json: @recipes, root: API_ROOT
   end
