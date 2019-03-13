88d183952f4435e6fb5dfff940553067742228a7
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-03-12 20:35:43 -0700

    Refactor community endpoint

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -31,11 +31,7 @@ class API::V1::RecipesController < API::V1::APIController
   end
 
   def community
-    if params[:q].present?
-      @recipes = Recipe.retrieve_all(current_user).where(title: params[:q])
-    else
-      @recipes = Recipe.retrieve_all(current_user)
-    end
+    @recipes = Recipe.search(params[:q]).retrieve_all(current_user)
     render json: @recipes, root: API_ROOT
   end
 
