f65929ed74cb602f76272f78020194e69959365e
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-03-12 20:18:02 -0700

    Add retrieve_all method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   def self.retrieve_all(user=nil)
+    recipes = user ? Recipe.where.not(user: user) : Recipe.all
+    recipes.order(id: :desc)
   end
 end
