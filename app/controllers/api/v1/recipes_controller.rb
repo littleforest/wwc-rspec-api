dd98466831d5bf30a69a4a5f913fcf548caada59
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-03-12 19:55:06 -0700

    Move render statement

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -6,17 +6,14 @@ class API::V1::RecipesController < API::V1::APIController
   def community
     if params[:q].present? && current_user.nil?
       @recipes = Recipe.where(title: params[:q]).order(id: :desc)
-      render json: @recipes, root: API_ROOT
     elsif params[:q].present? && current_user.present?
       @recipes = Recipe.where(title: params[:q])
                        .where.not(user: current_user).order(id: :desc)
-      render json: @recipes, root: API_ROOT
     elsif current_user
       @recipes = Recipe.where.not(user_id: current_user.id).order(id: :desc)
-      render json: @recipes, root: API_ROOT
     else
       @recipes = Recipe.order(id: :desc)
-      render json: @recipes, root: API_ROOT
     end
+    render json: @recipes, root: API_ROOT
   end
 end
