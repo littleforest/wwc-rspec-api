1ba5851abe19f6a490440edf831c2ba45f736b0d
Jeanine Soterwood <jeanine@littleforestconsulting.com>
2019-03-24 15:05:16 -0700

    vcr example

--- spec/services/api/v1/add_to_mailing_list_spec.rb
+++ spec/services/api/v1/add_to_mailing_list_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe API::V1::AddToMailingList, type: :request do
+  let(:user) { create(:user) }
+  let(:service) { API::V1::AddToMailingList.new(user) }
+
+  context 'when successful' do
+    it 'returns true' do
+      VCR.use_cassette("mailing_list_success") do
+        expect(service.call).to be true
+      end
+    end
+  end
+
+  context 'when not successful' do
+    it 'returns false' do
+      VCR.use_cassette("mailing_list_failure") do
+        expect(service.call).to be false
+      end
+    end
+  end
 end
